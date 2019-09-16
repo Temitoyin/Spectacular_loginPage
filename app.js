8177e79d25ca3c4b252f91c381f338d2bf540e78
elijahscriptdev <elijahobominuru@gmail.com>
2019-09-16 22:59:27 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+// sendind data to the database
+    //posting to backend
+    $(document).ready(function(){
+        var $firstName = $("#firstName");
+        var $lastName = $("#lastName");
+        var $email = $("#staticEmail");
+        var $userName = $("#userName");
+        var $password = $("#inputPassword");
+        
+      $('.add').on('click', function(e){
+        e.preventDefault();
+        alert("i see tasha");
+            $.ajax({
+          type: 'POST',
+          url: 'http://localhost:3000/users/',
+          data: requests = {
+            "firstName": $firstName.val(),
+            "lastName": $lastName.val(),
+            "email": $email.val(),
+            "userName": $userName.val(),
+            "password": $password.val(),
+        },
+          success: function(){
+            alert('form has been submitted');
+            window.location.href = "login.html";
+          },
+          error: function () {
+            alert('error');
+        }
+        });
+      });
+    });
+
+
+
+
+
